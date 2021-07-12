b91ab2df8672a7bcbc54f31a3fd310133e3afc9a
Your Name <you@example.com>
2021-07-12 21:12:17 +0300

    first commit

--- login.php
+++ login.php
@@ -1,28 +1,6 @@
 <?php
 $connection = mysqli_connect('localhost', 'root', 'root', 'test_task', '3306', '',);
 
-function get_client_ip()
-{
-    $ipaddress = '';
-    if (isset($_SERVER['HTTP_CLIENT_IP']))
-        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-    else if (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    else if (isset($_SERVER['HTTP_X_FORWARDED']))
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-    else if (isset($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']))
-        $ipaddress = $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
-    else if (isset($_SERVER['HTTP_FORWARDED_FOR']))
-        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-    else if (isset($_SERVER['HTTP_FORWARDED']))
-        $ipaddress = $_SERVER['HTTP_FORWARDED'];
-    else if (isset($_SERVER['REMOTE_ADDR']))
-        $ipaddress = $_SERVER['REMOTE_ADDR'];
-    else
-        $ipaddress = 'UNKNOWN';
-    return $ipaddress;
-}
-
 $user_ip = $_GET['user_ip'];
 $date_time = $_GET['date_time'];
 $button_id = $_GET['button_id'];
@@ -46,22 +24,18 @@ $array = [
 ];
 
 $createtable = "";
+$create = "CREATE TABLE IF NOT EXISTS tasks ( task_id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255) NOT NULL, start_date DATE, due_date DATE, status TINYINT NOT NULL, priority TINYINT NOT NULL, description TEXT, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP )  ENGINE=INNODB";
 
 $query1 = "sql query";
 $query2 = "sql query";
 $query3 = "sql query";
 $query4 = "sql query";
 
+$connection->query($create);
 $connection->query($query1);
 $connection->query($query2);
 $connection->query($query3);
 $connection->query($query4);
-
-array_diff_assoc();
-
-
-in_array('');;
-
 
 $array['user_ip'];
 $array['date_time'];
@@ -92,6 +66,35 @@ function yyyy_mm_dd_click()
 {
     return 'yyyy_mm_dd_click';
 }
+function get_client_ip()
+{
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if (isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if (isset($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
+    else if (isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if (isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if (isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+function writelog(){ }
+
+
+
+get_client_ip();
+yyyy_mm_dd_click();
+writelog();
+
 ?>
 
 <!DOCTYPE html>
